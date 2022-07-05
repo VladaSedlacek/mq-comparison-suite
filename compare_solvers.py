8806df2fe2e90c41cf09e2b526bdbe92b921ea48
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2022-07-05 15:07:21 +0200

    Add basic logging.

--- compare_solvers.py
+++ compare_solvers.py
@@ -1,5 +1,11 @@
+from pathlib import Path
 from subprocess import Popen
 import click
+
+
+def print_and_log(f, msg):
+    print(msg)
+    f.write(msg + "\n\n")
 
 
 @ click.command()
@@ -10,22 +16,25 @@ def main(o2_lb, o2_ub, runs):
     solvers = ['xl', 'crossbred', 'mq', 'libfes', 'wdsat', 'cms']
     q_range = [2, 4, 8, 16]
     o2_range = range(o2_lb, o2_ub)
-    for (i, solver) in enumerate(solvers):
-        for seed in range(runs):
-            for q in q_range:
-                for o2 in o2_range:
-                    m = 2 * o2
-                    n = 3 * o2
-                    solve_command = f"time sage rainbow_attacks.sage --seed {seed} --q {q} --o2 {o2} --m {m} --n {n} --solver {solver}"
-                    if i > 0:
-                        solve_command += " --solve_only"
-                    print("\n\n" + "*" * 100)
-                    print("Executing command:", solve_command)
-                    try:
-                        Popen(solve_command, shell=True).wait()
-                    except Exception as e:
-                        print(e)
-                        continue
+    log_path = Path("comparison_log.txt")
+    with open(log_path, 'w') as f:
+        for (i, solver) in enumerate(solvers):
+            for seed in range(runs):
+                for q in q_range:
+                    for o2 in o2_range:
+                        m = 2 * o2
+                        n = 3 * o2
+                        solve_command = f"time sage rainbow_attacks.sage --seed {seed} --q {q} --o2 {o2} --m {m} --n {n} --solver {solver}"
+                        if i > 0:
+                            solve_command += " --solve_only"
+                        solve_command += " | tee -a " + str(log_path)
+                        msg = "\n\n" + "*" * 100 + "\nExecuting command: " + solve_command
+                        print_and_log(f, msg)
+                        try:
+                            Popen(solve_command, shell=True).wait()
+                        except Exception as e:
+                            print_and_log(f, e)
+                            continue
 
 
 if __name__ == '__main__':
