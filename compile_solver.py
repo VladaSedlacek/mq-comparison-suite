30eceb76c2752cd42ee3de7223904b75f954aaed
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2022-09-09 16:17:52 +0200

    Externalize solver compilations.

---
+++ compile_solver.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from subprocess import Popen
+import click
+import json
+
+
+def create_wdsat_config(wdsat_path, M, N):
+    with open(Path(wdsat_path, "src", "config.h"), 'w') as file:
+        file.write("""
+//enable the XG-ext module (must use anf as input)
+# define __XG_ENHANCED__
+
+//find all solutions instead of only one
+# define __FIND_ALL_SOLUTIONS__
+
+/** Rainbow : N={0} M={1} **/
+# ifdef __XG_ENHANCED__
+# define __MAX_ANF_ID__ {2} // make it +1
+# define __MAX_DEGREE__ 3 // make it +1
+# endif
+# define __MAX_ID__ {3}
+# define __MAX_BUFFER_SIZE__ 200000
+# define __MAX_EQ__ {5}
+# define __MAX_EQ_SIZE__ 4 //make it +1
+# define __MAX_XEQ__ {1}
+# define __MAX_XEQ_SIZE__ {4}""".format(N, M, N + 1, int(N*(N-1)/2), N * (N + 1), int(M*(M-1)/2)))
+
+
+def check_params(status_path, q, m, n):
+    if status_path.exists():
+        with open(status_path, 'r') as f:
+            params = json.load(f)
+            if params['q'] == q and params['m'] == m and params['n'] == n:
+                return True
+    return False
+
+
+@ click.command()
+@ click.option('--solver', type=click.Choice(['xl', 'wdsat'], case_sensitive=False), help='the external solver to be compiled')
+@ click.option('--q', help='field characteristic - needed for XL compilation', type=int)
+@ click.option('--m', help='number of equations - needed for XL and WDSAT compilation', type=int)
+@ click.option('--n', help='number of variables - needed for XL and WDSAT compilation', type=int)
+@ click.option('--xl_path', default=Path("..", "xl"), help='the path the XL solver folder: http://polycephaly.org/projects/xl', type=str)
+@ click.option('--wdsat_path', default=Path("..", "WDSat"), help='the path the WDSat solver folder: https://github.com/mtrimoska/WDSat', type=str)
+def main(solver, q, m, n, xl_path, wdsat_path,):
+    if not solver:
+        print("Please specify a solver.")
+        exit()
+
+    if solver == 'xl':
+        xl_status_path = Path("xl_status.json")
+        compiled = check_params(xl_status_path, q, m, n)
+        if compiled and Path(xl_path, "xl").exists():
+            print("\nThe XL solver is already compiled.")
+        else:
+            make_cmd = f"make -C {str(xl_path)} clean && make -C {str(xl_path)} Q={q} M={m} N={n} -Wno-unused-result -Wno-class-memaccess"
+            print("\nCompiling the XL solver...")
+            Popen(make_cmd, shell=True).wait()
+            with open(xl_status_path, 'w') as f:
+                params = {'q': q, 'm': m, 'n': n}
+                json.dump(params, f)
+
+    if solver == 'wdsat':
+        wdsat_status_path = Path("wdsat_status.json")
+        compiled = check_params(wdsat_status_path, q, m, n)
+        if compiled and Path(wdsat_path, "wdsat_solver").exists():
+            print("\nThe WDSat solver is already compiled.")
+        else:
+            create_wdsat_config(wdsat_path, m, n)
+            suppressor_flag = ""
+            src_path = str(Path(wdsat_path, "src"))
+            make_cmd = f"make -C {src_path} {suppressor_flag}"
+            print(make_cmd)
+            print("\nCompiling the WDSat solver...")
+            Popen(make_cmd, shell=True).wait()
+            with open(wdsat_status_path, 'w') as f:
+                params = {'q': q, 'm': m, 'n': n}
+                json.dump(params, f)
+
+
+if __name__ == '__main__':
+    main()
--- invoke_solver.py
+++ invoke_solver.py
@@ -41,7 +41,8 @@ def create_wdsat_config(wdsat_path, M, N):
 @ click.option('--wdsat_path', default=Path("..", "WDSat"), help='the path the WDSat solver folder: https://github.com/mtrimoska/WDSat', type=str)
 @ click.option('--cms_path', default=Path("..", "cryptominisat", "build"), help='the path the CMS solver folder: https://github.com/mtrimoska/WDSat', type=str)
 @ click.option('-h', '--inner_hybridation', default="-1", help='the number of variable that are not guessed in MQ', type=int)
-def main(equations_path, log_path, q, m, n, xl_path, crossbred_path, mq_path, libfes_path, wdsat_path, cms_path, solver, inner_hybridation):
+@ click.option('--precompiled', default=False, is_flag=True, help='indicates if all relevant solvers are already compiled w.r.t. the parameters')
+def main(equations_path, log_path, q, m, n, xl_path, crossbred_path, mq_path, libfes_path, wdsat_path, cms_path, solver, inner_hybridation, precompiled):
     if not solver:
         print("Please specify a solver.")
         exit()
@@ -52,74 +53,58 @@ def main(equations_path, log_path, q, m, n, xl_path, crossbred_path, mq_path, li
 
     current_path = Path().cwd()
 
-    def check_params(path, q, m, n):
-        if path.exists():
-            with open(path, 'r') as f:
-                params = json.load(f)
-                if params['q'] == q and params['m'] == m and params['n'] == n:
-                    return True
-        return False
-
     if solver == 'xl':
-        xl_status_path = Path("xl_status.json")
-        compiled = check_params(xl_status_path, q, m, n)
-        if compiled and Path(xl_path, "xl").exists():
+        if precompiled and Path(xl_path, "xl").exists():
             print("\nThe XL solver is already compiled.")
         else:
-            make_command = "make -C {0} clean && make -C {0} Q={1} M={2} N={3} -Wno-unused-result -Wno-class-memaccess".format(
-                str(xl_path), str(q), str(m), str(n)) + " > " + str(log_path)
-            print("\nCompiling the XL solver...")
-            Popen(make_command, shell=True).wait()
-            with open(xl_status_path, 'w') as f:
-                params = {'q': q, 'm': m, 'n': n}
-                json.dump(params, f)
+            make_cmd = f"python3 compile_solver.py --solver xl --q {q} --m {m} --n {n} --xl_path {xl_path}"
+            Popen(make_cmd, shell=True).wait()
         print("\nStarting the XL solver...")
-        xl_solve_command = "{} --challenge {} --all".format(
+        xl_solve_cmd = "{} --challenge {} --all".format(
             str(Path(xl_path, "xl")), str(equations_path)) + " | tee -a " + str(log_path)
-        Popen(xl_solve_command, shell=True).wait()
+        Popen(xl_solve_cmd, shell=True).wait()
 
     if solver == 'crossbred':
         print("\nStarting the crossbred solver...")
         os.chdir(crossbred_path)
-        crossbred_solve_command = "./solve.py -d 3 -k 16 -t 20 -v -o {} {}".format(
+        crossbred_solve_cmd = "./solve.py -d 3 -k 16 -t 20 -v -o {} {}".format(
             str(Path(current_path, log_path)), str(Path(current_path, equations_path)))
-        Popen(crossbred_solve_command, shell=True).wait()
+        Popen(crossbred_solve_cmd, shell=True).wait()
         os.chdir(current_path)
 
     if solver == 'mq':
         print("\nStarting the MQ solver...")
         inner_hybridation_arg = " --inner-hybridation " + \
             str(inner_hybridation) if inner_hybridation != -1 else ""
-        mq_solve_command = "{}{} < {}".format(
+        mq_solve_cmd = "{}{} < {}".format(
             str(Path(mq_path, "monica_vector")), inner_hybridation_arg, str(equations_path)) + " | tee " + str(log_path)
-        Popen(mq_solve_command, shell=True).wait()
+        Popen(mq_solve_cmd, shell=True).wait()
 
     if solver == 'libfes':
         print("\nStarting the libfes solver...")
-        mq_solve_command = "{} < {}".format(
+        mq_solve_cmd = "{} < {}".format(
             str(Path(libfes_path, "benchmark", "demo")), str(equations_path)) + " | tee " + str(log_path)
-        Popen(mq_solve_command, shell=True).wait()
+        Popen(mq_solve_cmd, shell=True).wait()
 
     if solver == 'wdsat':
-        print("\nCompiling the WDSat solver...")
-        create_wdsat_config(wdsat_path, m, n)
-        suppressor_flag = "-n"
-        make_command = "make -C {0} {1} > {2}".format(
-            str(Path(wdsat_path, "src")), suppressor_flag, str(log_path))
-        Popen(make_command, shell=True).wait()
+        if precompiled and Path(wdsat_path, "wdsat_solver").exists():
+            print("\nThe WDSat solver is already compiled.")
+        else:
+            make_cmd = f"python3 compile_solver.py --solver wdsat --q {q} --m {m} --n {n} --wdsat_path {wdsat_path}"
+            Popen(make_cmd, shell=True).wait()
         print("\nStarting the WDSat solver...")
-        wdsat_solve_command = "{} -i {}".format(
+        wdsat_solve_cmd = "{} -i {}".format(
             str(Path(wdsat_path, "wdsat_solver")), str(equations_path) +
             " | tee -a " + str(log_path))
-        Popen(wdsat_solve_command, shell=True).wait()
+        Popen(wdsat_solve_cmd, shell=True).wait()
 
     if solver == 'cms':
         Popen(" > {}".format(str(log_path)), shell=True).wait()
         print("\nStarting the CryptoMiniSat solver...")
-        cms_solve_command = "{} --verb 0 {}".format(
+        cms_solve_cmd = "{} --verb 0 {}".format(
             str(Path(cms_path, "cryptominisat5")), str(equations_path) +
             " | tee -a " + str(log_path))
-        Popen(cms_solve_command, shell=True).wait()
+        Popen(cms_solve_cmd, shell=True).wait()
 
 
 if __name__ == '__main__':
